bf4dffd96b5d8b8398366f6d580a70818333c0aa
nearbyjustine <nearbyjustine@gmail.com>
2025-03-20 13:02:12 +0800

    added default link for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,20 @@
+import DownloadButton from "./DownloadButton";
+import { Button } from "./ui/button";
+
 function Navbar() {
 	return (
-		<div className="flex bg-amber-200 justify-between px-8 py-4">
+		<div className="flex justify-between px-8 py-4">
 			<div>Logo</div>
 			<div className="flex gap-8">
-				<div>About Me</div>
-				<div>Projects</div>
-				<div>Skills</div>
-				<div>Contact Me</div>
+				<a href="#">About Me</a>
+				<a href="#">Projects</a>
+				<a href="#">Skills</a>
+				<a href="#">Contact Me</a>
 			</div>
-			<div>Resume</div>
+			<div>
+				<Button>Hi</Button>
+				<DownloadButton text="Resume" />
+			</div>
 		</div>
 	);
 }
