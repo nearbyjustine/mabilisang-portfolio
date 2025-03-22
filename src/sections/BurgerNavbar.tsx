cae2be48d78f09a15c215a89122a10fdb643845e
nearbyjustine <nearbyjustine@gmail.com>
2025-03-22 09:37:38 +0800

    added focus and updated imports

--- src/sections/BurgerNavbar.tsx
+++ src/sections/BurgerNavbar.tsx
@@ -1,6 +1,6 @@
 import DownloadButton from "@/components/DownloadButton";
 import { cn } from "@/lib/utils";
-import type { NavbarProps } from "@/types/navbar";
+import type { NavbarProps } from "@/types/props";
 import { RxCross1 } from "react-icons/rx";
 
 function BurgerNavbar({
@@ -31,31 +31,31 @@ function BurgerNavbar({
 			</div>
 			<div className="flex-1 flex flex-col gap-6 items-center justify-center text-white">
 				<a
-					className="py-2 px-3 rounded-lg hover:bg-slate-800 transition-all"
+					className="py-2 px-3 rounded-lg hover:bg-slate-800 focus:bg-slate-800 transition-all"
 					href="#"
 				>
 					Experience
 				</a>
 				<a
-					className="py-2 px-3 rounded-lg hover:bg-slate-800 transition-all"
+					className="py-2 px-3 rounded-lg hover:bg-slate-800 focus:bg-slate-800 transition-all"
 					href="#"
 				>
 					About Me
 				</a>
 				<a
-					className="py-2 px-3 rounded-lg border-b-0 hover:bg-slate-800 transition-all"
+					className="py-2 px-3 rounded-lg border-b-0 hover:bg-slate-800 focus:bg-slate-800 transition-all"
 					href="#"
 				>
 					Projects
 				</a>
 				<a
-					className="py-2 px-3 rounded-lg hover:bg-slate-800 transition-all"
+					className="py-2 px-3 rounded-lg hover:bg-slate-800 focus:bg-slate-800 transition-all"
 					href="#"
 				>
 					Skills
 				</a>
 				<a
-					className="py-2 px-3 rounded-lg hover:bg-slate-800 transition-all"
+					className="py-2 px-3 rounded-lg hover:bg-slate-800 focus:bg-slate-800 transition-all"
 					href="#"
 				>
 					Contact Me
