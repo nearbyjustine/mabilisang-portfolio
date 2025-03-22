d3074d5351ac411e95f6ccc57bb50a7fd1b0654a
nearbyjustine <nearbyjustine@gmail.com>
2025-03-22 09:36:59 +0800

    added eventListener for window resize

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,18 @@ import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { useEffect, useState } from "react";
 import { cn } from "./lib/utils";
 import BurgerNavbar from "./sections/BurgerNavbar";
+import Experiences from "./sections/Experiences";
 
 function App() {
-	const [burgerClicked, setBurgerClicked] = useState(true);
+	const [burgerClicked, setBurgerClicked] = useState(false);
 	const [inMobileView, setInMobileView] = useState(false);
 	useEffect(() => {
 		const inMobile = () => {
 			// eslint-disable-next-line @typescript-eslint/no-unused-expressions
 			window.innerWidth < 640 ? setInMobileView(true) : setInMobileView(false);
-			return;
 		};
 		inMobile();
+		addEventListener("resize", inMobile);
 	}, []);
 	return (
 		<>
@@ -32,16 +33,17 @@ function App() {
 					/>
 
 					<Hero />
-					<div className="flex 2xl:px-20 px-10">
+					<div className="flex 2xl:px-20 px-10 items-center my-10">
 						{/* Socials */}
-						<div className="flex gap-4 md:-mt-[20px] mt-5">
+						<div className="flex gap-4 md:-mt-[20px]">
 							<SquareButton icon={FaLinkedin} />
 							<SquareButton icon={FaFacebook} />
 							<SquareButton icon={FaInstagram} />
 						</div>
-						<div className="md:block hidden h-1 bg-black w-full ml-[120px]" />
+						<div className="md:block md:h-1 h-0.5 bg-black w-full md:ml-[120px] ml-5" />
 					</div>
 				</div>
+				<Experiences className="flex flex-col justify-between 2xl:px-20 2xl:pt-20 px-10 py-10 items-center md:gap-8 gap-4" />
 			</div>
 		</>
 	);
