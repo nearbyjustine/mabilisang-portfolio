7e1643d1c5ac90d2af6146e68db62bf51112b810
nearbyjustine <nearbyjustine@gmail.com>
2025-03-28 13:57:54 +0800

    responsive about me

--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -1,13 +1,13 @@
 function AboutMe() {
 	return (
 		<section className="2xl:px-20 px-10 2xl:py-20 py-10" id="me">
-			<div className="flex md:flex-row flex-col gap-6 md:gap-12">
+			<div className="flex lg:flex-row flex-col items-center justify-center gap-6 md:gap-12">
 				<img
 					className="w-[500px] md:[1200px] rounded-full border-2 border-slate-500"
 					src="me-sketch-circle.webp"
 					alt="placeholder"
 				/>
-				<div className="w-full flex flex-col items-center justify-center">
+				<div className="w-full md:w-[500px] flex flex-col items-center justify-center">
 					<h1 className="md:text-4xl text-2xl">More about me...</h1>
 					<p className="mt-4 text-slate-700 md:text-base text-sm">
 						I’m a full-stack developer who loves creating seamless,
